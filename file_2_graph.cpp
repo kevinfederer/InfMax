d480ff9600f0fd7d1e223dc4c1792dd7ad6fe2be
anryyang <anryyang@gmail.com>
2016-10-22 16:13:16 +0800

    add

---
+++ file_2_graph.cpp
@@ -0,0 +1,33 @@
+/*************************************************************************
+    > File Name: file_2_graph.cpp
+    > Author: anryyang
+    > Mail: anryyang@gmail.com 
+    > Created Time: Sat Oct 22 16:02:51 2016
+ ************************************************************************/
+
+#include "file_2_graph.h"
+
+FileToGraph::~FileToGraph(){}
+
+void FileToGraph::convert(){
+
+}
+
+void FileToGraph::get_graph(Graph &g){
+
+}
+
+void FileToGraph::get_users(Users &u){
+
+}
+
+double FileToGraph::get_weight(){
+
+}
+
+double FileToGraph::get_opinion(){
+
+}
+
+
+
